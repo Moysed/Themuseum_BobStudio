8750afaf3ae01ce7731ecdf02affc94df0062f0a
NEUROMANCER3599 <61156024+NEUROMANCER3599@users.noreply.github.com>
2023-08-12 22:12:03 +0700

    Simple Puzzle and Hint Note

--- Themuseum/Game1.cs
+++ Themuseum/Game1.cs
@@ -12,12 +12,15 @@ namespace Themuseum
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
         private AnimatedTexture Character;
+        private AnimatedTexture Ghost;
+        private AnimatedTexture MagicCircle;
+        private AnimatedTexture MagicCrystal;
         private Texture2D Key;
         private Texture2D ExitDoor;
         private const float Rotation = 0;
         private const float Scale = 1.0f;
         private const float Depth = 0.5f;
-        private int speed = 2;
+        private int speed = 3;
         private SpriteFont _font;
 
         private int Frames = 4;
@@ -51,17 +54,26 @@ namespace Themuseum
 
         private int Doorspriteframe = 8;
         private Vector2 Doorpos;
+        private Vector2 Circlepos;
+        private int CircleType = 1;
+        private bool CircleActive = true;
+
+        private Vector2 Notepos;
+        
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
             Character = new AnimatedTexture(Vector2.Zero, Rotation, Scale, Depth);
+            Ghost = new AnimatedTexture(Vector2.Zero, Rotation, Scale, Depth);
+            MagicCircle = new AnimatedTexture(Vector2.Zero, Rotation, Scale, Depth);
+            MagicCrystal = new AnimatedTexture(Vector2.Zero, Rotation, Scale, Depth);
             Content.RootDirectory = "Content";
             IsMouseVisible = true;
         }
 
         protected override void Initialize()
         {
-            Tileset = Content.Load<Texture2D>("placeholder_tileset");
+            Tileset = Content.Load<Texture2D>("040-Tower02");
             Key = Content.Load<Texture2D>("key-white");
             _font = Content.Load<SpriteFont>("Keycollect");
             ExitDoor = Content.Load<Texture2D>("placeholderdoor");
@@ -93,6 +105,9 @@ namespace Themuseum
 
             Doorpos = new Vector2(32 * 20, 0);
             keyPos = new Vector2(32 * 5, 32 * 12);
+            Circlepos = new Vector2(keyPos.X - 16, keyPos.Y - 20);
+            Notepos = new Vector2(32 * 20, 256);
+            CircleType = r.Next(1, 4);
             //Character Position Initial
             CharPos = new Vector2(GraphicsDevice.Viewport.Width / 2, GraphicsDevice.Viewport.Height / 2);
             base.Initialize();
@@ -103,8 +118,10 @@ namespace Themuseum
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
             Character.Load(Content, "placeholdersprite", Frames, FramesRow, FramesPerSec);
+            Ghost.Load(Content, "054-Undead04", Frames, FramesRow, FramesPerSec);
+            MagicCircle.Load(Content, "199-Support07", Frames, FramesRow, FramesPerSec);
+            MagicCrystal.Load(Content, "198-Support06", Frames, FramesRow, FramesPerSec);
 
-            
         }
 
         protected override void Update(GameTime gameTime)
@@ -113,7 +130,12 @@ namespace Themuseum
             Rectangle charRectangle = new Rectangle((int)CharPos.X, (int)CharPos.Y, 32, 48);
             Rectangle KeyRectangle = new Rectangle((int)keyPos.X, (int)keyPos.Y, 24, 24);
             Rectangle DoorRectangle = new Rectangle((int)Doorpos.X, (int)Doorpos.Y, 32, 64);
-           
+            Rectangle CircleBox = new Rectangle((int)Circlepos.X, (int)Circlepos.Y, 64, 64);
+            Rectangle Crystal_1 = new Rectangle(64, 32, 32, 64);
+            Rectangle Crystal_2 = new Rectangle(64 + 32 + 10, 32,32,64);
+            Rectangle Crystal_3 = new Rectangle(64 + 64 + 20, 32, 32, 64);
+            Rectangle Crystal_4 = new Rectangle(64 + 96 + 30, 32, 32, 64);
+            Rectangle NoteBox = new Rectangle((int)Notepos.X, (int)Notepos.Y, 32, 32);
             /*else if (charRectangle.Intersects(KeyRectangle) == false)
             {
                 personHit = false;
@@ -129,28 +151,34 @@ namespace Themuseum
 
             if (charRectangle.Intersects(KeyRectangle) == true)
             {
-                Textcolor = Color.White;
-                displaytext = "Press E to Interact";
+                //Textcolor = Color.White;
+                //displaytext = "Press E to Interact";
                 /*
                 personHit = true;
                 keyPos.X = r.Next(_graphics.GraphicsDevice.Viewport.Bounds.Left + 32, _graphics.GraphicsDevice.Viewport.Bounds.Right);
                 keyPos.Y = r.Next(_graphics.GraphicsDevice.Viewport.Bounds.Top + 72, _graphics.GraphicsDevice.Viewport.Bounds.Bottom);
                 score += 1;
                 */
-                if (_keyboardState.IsKeyDown(Keys.E))
+                if (_keyboardState.IsKeyDown(Keys.E) && CircleActive == false)
                 {
                     HasKey = true;
                     keyPos = new Vector2(10000, 10000);
                     Textcolor = Color.Gold;
                     displaytext = "Key Collected";
                 }
+                else if(_keyboardState.IsKeyDown(Keys.E) && CircleActive == true)
+                {
+                    
+                    Textcolor = Color.LightPink;
+                    displaytext = "The magic circle is preventing the key from moving";
+                }
                     
             }
-
+          
             if (charRectangle.Intersects(DoorRectangle) == true && IsUnlocked == false)
             {
-                Textcolor = Color.White;
-                displaytext = "Press E to Interact";
+                //Textcolor = Color.White;
+                //displaytext = "Press E to Interact";
                 if (_keyboardState.IsKeyDown(Keys.E) && HasKey == false)
                 {
                     Textcolor = Color.Red;
@@ -169,6 +197,7 @@ namespace Themuseum
                 Textcolor = Color.LightGreen;
                 displaytext = "The door is opened";
             }
+            
 
             if (_keyboardState.IsKeyDown(Keys.A))
             {
@@ -187,6 +216,75 @@ namespace Themuseum
             {
                     CharPos.Y += speed;
             }
+
+            if(charRectangle.Intersects(Crystal_1) && CircleActive == true)
+            {
+                if (_keyboardState.IsKeyDown(Keys.E) && CircleType == 1)
+                {
+                    Textcolor = Color.Aqua;
+                    displaytext = "Barrier circle deactivated";
+                    CircleActive = false;
+                }
+                else if (_keyboardState.IsKeyDown(Keys.E))
+                {
+                    Textcolor = Color.Red;
+                    displaytext = "You choose wrong, prepare for consequences!";
+                }
+            }
+            if (charRectangle.Intersects(Crystal_2) && CircleActive == true)
+            {
+                if (_keyboardState.IsKeyDown(Keys.E) && CircleType == 2)
+                {
+                    Textcolor = Color.Aqua;
+                    displaytext = "Barrier circle deactivated";
+                    CircleActive = false;
+                }
+                else if (_keyboardState.IsKeyDown(Keys.E))
+                {
+                    Textcolor = Color.Red;
+                    displaytext = "You choose wrong, prepare for consequences!";
+                }
+            }
+            if (charRectangle.Intersects(Crystal_3) && CircleActive == true)
+            {
+                if (_keyboardState.IsKeyDown(Keys.E) && CircleType == 3)
+                {
+                    Textcolor = Color.Aqua;
+                    displaytext = "Barrier circle deactivated";
+                    CircleActive = false;
+                }
+                else if (_keyboardState.IsKeyDown(Keys.E))
+                {
+                    Textcolor = Color.Red;
+                    displaytext = "You choose wrong, prepare for consequences!";
+                }
+            }
+            if (charRectangle.Intersects(Crystal_4) && CircleActive == true)
+            {
+                if (_keyboardState.IsKeyDown(Keys.E) && CircleType == 4)
+                {
+                    Textcolor = Color.Aqua;
+                    displaytext = "Barrier circle deactivated";
+                    CircleActive = false;
+                }
+                else if(_keyboardState.IsKeyDown(Keys.E))
+                {
+                    Textcolor = Color.Red;
+                    displaytext = "You choose wrong, prepare for consequences!";
+                }
+            }
+
+            if (CircleActive == false)
+            {
+                Circlepos = new Vector2(10000, 10000);
+            }
+
+            if (charRectangle.Intersects(NoteBox) && _keyboardState.IsKeyDown(Keys.E))
+            {
+                Textcolor = Color.Orange;
+                displaytext = "Hint:\n\n G = Y \n\n R = B \n\n B = R \n\n Y = G";
+            }
+            
             /*
             //Creating Collision (Upperwall) | Experimental System
             for (int i = 0; i < ((int)GraphicsDevice.Viewport.Width / 32); i++)
@@ -224,7 +322,10 @@ namespace Themuseum
             {
                 CharPos.Y -= 3;
             }
+
+            MagicCircle.UpdateFrame(elapsed);
             Character.UpdateFrame(elapsed);
+            MagicCrystal.UpdateFrame(elapsed);
             base.Update(gameTime);
         }
 
@@ -274,13 +375,21 @@ namespace Themuseum
                 }
             }
 
-           
+            //MagicCircle
+            MagicCircle.DrawFrame(_spriteBatch, Circlepos,CircleType);
             //Key
             _spriteBatch.Draw(Key, keyPos, new Rectangle(0, 0, 32, 32), Color.White);
             //Door
             _spriteBatch.Draw(ExitDoor, Doorpos, new Rectangle(6 * 32, Doorspriteframe * 32, 32, 64), Color.White);
-            
 
+            //MagicCrystal
+            MagicCrystal.DrawFrame(_spriteBatch,new Vector2(64,32),1);
+            MagicCrystal.DrawFrame(_spriteBatch, new Vector2(64 + 32 + 10, 32), 2);
+            MagicCrystal.DrawFrame(_spriteBatch, new Vector2(64 + 64 + 20, 32), 3);
+            MagicCrystal.DrawFrame(_spriteBatch, new Vector2(64 + 96 + 30, 32), 4);
+
+            //Note
+            _spriteBatch.Draw(Tileset, Notepos, new Rectangle(5 * 32, 27 * 32, 32, 32), Color.White);
             //Player Animation
             if (_keyboardState.IsKeyDown(Keys.A))
             {
@@ -308,7 +417,8 @@ namespace Themuseum
             }
 
             //Text
-            _spriteBatch.DrawString(_font, displaytext, new Vector2(GraphicsDevice.Viewport.Bounds.Left + 16, GraphicsDevice.Viewport.Bounds.Top), Textcolor, 0, Vector2.Zero, 0.75f, SpriteEffects.None, 1);
+            //_spriteBatch.DrawString(_font, displaytext, new Vector2(GraphicsDevice.Viewport.Bounds.Left + 16, GraphicsDevice.Viewport.Bounds.Top), Textcolor, 0, Vector2.Zero, 0.75f, SpriteEffects.None, 1);
+            _spriteBatch.DrawString(_font, displaytext, new Vector2(CharPos.X - 64, CharPos.Y - 48), Textcolor, 0, Vector2.Zero, 0.75f, SpriteEffects.None, 1);
 
             _spriteBatch.End();
             base.Draw(gameTime);
